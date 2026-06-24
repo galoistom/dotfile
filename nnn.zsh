6416b7f90647f1bb28f0076567e74b5ddd2c62ab
galoistom <liug17969@gmail.com>
2026-06-24 19:47:03 +0800

    updates 2026-6-24

--- nnn.zsh
+++ nnn.zsh
@@ -1,6 +1,6 @@
 export PATH="$HOME/.config/nnn/bin:$PATH"
 export NNN_PLUG='x:extract;v:preview-tui;f:fzopen;m:nmount;c:compress;p:screenshot-past'
 export NNN_FIFO=/tmp/nnn.fifo
-export NNN_BMS="b:/home/galoistom/math-note/notes/Books"
+export NNN_BMS="b:/home/galoistom/math-note/notes/Books;d:/home/galoistom/Downloads"
 export NNN_FCOLORS="D4DEB778E79F9F67D2E5E5D2"
 
